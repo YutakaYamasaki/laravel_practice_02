65f3b3ea4dfa07586c0576fcd9d0cb919f55e787
YutakaYamasaki <uminchuvirtual@gmail.com>
2018-12-25 12:32:13 +0800

    episode 13

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -24,7 +24,7 @@ class ProjectsController extends Controller
 
     public function update($id)
     {
-        $project =Project::find($id);
+        $project =Project::findOrFail($id);
 
         $project->title = request('title');
         $project->description = request('description');
@@ -36,12 +36,14 @@ class ProjectsController extends Controller
 
     public function edit($id)
     {
-        $project =Project::find($id);
+        $project =Project::findOrFail($id);
         return view('projects.edit',compact('project'));
     }
 
-    public function destroy()
+    public function destroy($id)
     {
+        Project::findOrFail($id)->delete();
+        return redirect('/projects');
 
     }
 
--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -3,9 +3,9 @@
 
     <h1 class="title">Edit Project</h1>
 
-    <form action="/projects/{{ $project->id }}" method="POST">
-        {{ method_field('PATCH') }}
-        {{ csrf_field() }}
+    <form action="/projects/{{ $project->id }}" method="POST" style="margin-bottom: 1">
+        @method('PATCH')
+        @csrf
         <div class="field">
             <label class="label" for="title">Title</label>
 
@@ -22,6 +22,16 @@
         <div class="field">
                 <div class="control">
                     <button type="submit" class="button is-link">Update Project</button>
+                </div>
+        </div>
+    </form>
+    <form method="POST" action="/projects/{{ $project->id }}">
+        @method('DELETE')
+        @csrf
+
+        <div class="field">
+                <div class="control">
+                    <button type="submit" class="button">Delete Project</button>
                 </div>
         </div>
     </form>
